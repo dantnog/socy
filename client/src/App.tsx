cef1ad631708d277b5ccab1cf5b4f3883ab59bba
Daniel Nogueira <nogueira.ntd@gmail.com>
2022-11-26 14:47:50 -0300

    feat: Redirect logged user to home

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,18 @@ import Signup from './pages/Signup'
 import Welcome from './pages/Welcome'
 
 function App() {
+  function isLogged() {
+    const re = /jwt/g
+    return re.test(document.cookie)
+  }
+
   return (
     <div className="w-screen min-h-screen bg-white dark:bg-gray-900 text-gray-600 dark:text-gray-400">
       <Routes>
-        <Route path="/" element={ <Welcome /> } />
+        <Route path="/" element={  !isLogged() ? <Welcome /> : <Navigate to="/home" /> } />
         <Route path="/home" element={ <div>HOME</div> } />
-        <Route path="/login" element={ <Login /> } />
-        <Route path="/signup" element={ <Signup /> } />
+        <Route path="/login" element={ !isLogged() ? <Login /> : <Navigate to="/home" /> } />
+        <Route path="/signup" element={ !isLogged() ? <Signup /> : <Navigate to="/home" /> } />
       </Routes>
       <ToastContainer></ToastContainer>
     </div>
